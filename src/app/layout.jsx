01cd61229234edae3c73fcffeea94574f20bd682
laperkins2 <67718788+laperkins2@users.noreply.github.com>
2024-10-13 16:41:58 -0500

    initial commit

---
+++ src/app/layout.jsx
@@ -0,0 +1,34 @@
+import localFont from 'next/font/local';
+import './globals.css';
+
+const geistSans = localFont({
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
+});
+const geistMono = localFont({
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
+});
+
+export const metadata = {
+  title: 'Shirts For Sale',
+  description: 'An online store for shirts for level 4 capstone',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <header className="my-18 text-center">Shirts For Sale</header>
+        <main className="min-h-screen">{children}</main>
+        <footer className="my-18 text-center">
+          &copy; Louis Perkins Capstone Level 4
+        </footer>
+      </body>
+    </html>
+  );
+}
